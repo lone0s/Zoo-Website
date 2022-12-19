10cc98dffe5b27c2382bf4a294ebb960953d0869
L337 <loneos1337@gmail.com>
2022-12-19 21:26:01 +0100

    base setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 8000;
+const hostname = '127.0.0.1';
+const whoami = 'root';
+const server = http.createServer((req,res) => {
+    res.end(`$ whoami\n$ ${whoami}`);
+}) ;
+
+server.listen(port, hostname, () => {
+    console.log(`Currently listening on ${hostname}:${port}`);
+})
+
