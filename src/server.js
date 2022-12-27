29fc277b958456f303d5b3ddaf0a235cdd6c4998
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2022-12-27 20:23:23 +0100

    Ajout de express, début de routing

--- src/server.js
+++ src/server.js
@@ -1,13 +1,36 @@
-const http = require('http');
-const port = 8000;
+/**Imports**/
+//import React from "react";
+//import ReactDOM from "react-dom";
+
+/**Consts decls**/
+const express = require("express")
+const app = express()
+//const router = app.Router()
+
+/**Routes decls**/
+//const {eventRouter} = require("./event-router")
+
+/**Server params**/
 const hostname = '127.0.0.1';
-const whoami = 'root';
+const port = 8000;
 
-const server = http.createServer((req,res) => {
-    res.end(`$ whoami\n$ ${whoami}`);
-}) ;
+/**Routes param**/
+/*router.get('/router-test', (req, res) => {
+    res.send('./router-test')
+})*/
 
-server.listen(port, hostname, () => {
+/**Routes**/
+app.get('/',
+    (req, res) => {
+        res.redirect('/acceuil')
+    }
+);
+
+app.get('/acceuil', (req, res) => {
+    res.send("Page d'acceuil")
+})
+
+app.listen(port, hostname, () => {
     console.log(`Currently listening on ${hostname}:${port}`);
 })
 
