3493b2af54444db7f6623ffec09c63e24f31f910
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2022-12-27 20:37:51 +0100

    Update acceuil.jsx

---
+++ src/dist/acceuil.jsx
@@ -0,0 +1,10 @@
+/**Imports**/
+import React from "react";
+import ReactDOM from "react-dom";
+
+/**Composant**/
+const Acceuil = <div>Voici la page d'acceuil</div>
+
+
+/**Render**/
+ReactDOM.render(Acceuil, document.getElementById("root"))
