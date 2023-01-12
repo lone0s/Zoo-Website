52e10c3aa9b09380dc3ed94e19d5f9c9f455b421
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2023-01-12 16:41:27 +0100

    Facotrisation de code

--- src/dist/MenuBar.jsx
+++ src/dist/MenuBar.jsx
@@ -1,7 +1,8 @@
 /**Imports**/
 import React from "react";
 import ReactDOM from "react-dom";
-import Roles from "./roles";
+import {Roles, getUser} from "./roles";
+import {Urls} from "../urls";
 
 /**Composant**/
 class MenuBar extends React.Component {
@@ -9,37 +10,25 @@ class MenuBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            connectedUser : {
-                id : 0,
-                ndc : "Steduthu_admin",
-                mdp : "1532",
-                role : Roles.Admin,
-            }
+            connectedUser : getUser()
         }
     }
 
-    componentDidMount() { //TODO : recuperer la personne connectee pour effectuer les affichages en consequence
-        /*fetch('/_api/connectedUser')
-            .then((res) => res.json())
-            .then((eventsReponse) => {
-                this.connectedUser = eventsReponse
-                this.setState({state : connectedUser : eventsReponse})
-            })
-        this.render()*/
+    componentDidMount() {
+        this.setState({state : {connectedUser : getUser()}})
+        this.render()
     }
 
     render(){
-        console.log("rendering")
-        console.log(this)
         return(
             <nav>
-                {window.location.pathname === "acceuil" ? <a/> : <a id="acceuil" className='menu_button' href="/acceuil">Acceuil&nbsp;|&nbsp;</a>}
-                <a id="zoo_map" className='menu_button' href="/zoo_map">Zoo map&nbsp;|&nbsp;</a>
-                {this.state.connectedUser === undefined ? <a id="connexion" className='menu_button' href='/connexion'>Connexion&nbsp;|&nbsp;</a> : <a/>}
-                {this.state.connectedUser === undefined ? <a id="inscription" className='menu_button' href='/inscription'>Inscription&nbsp;|&nbsp;</a> : <a/>}
+                {window.location.pathname === Urls.Acceuil ? <a/> : <button id="acceuil" className='menu_button' onClick={() => {window.location.href = Urls.Acceuil;}}>Acceuil;</button>}
+                <button id="zoo_map" className='menu_button' onClick={() => {window.location.href = Urls.ZooMap;}}>Zoo map</button>
+                {this.state.connectedUser === undefined ? <button id="connexion" className='menu_button' onClick={() => {window.location.href = Urls.Connexion;}}>Connexion</button> : <a/>}
+                {this.state.connectedUser === undefined ? <button id="inscription" className='menu_button' onClick={() => {window.location.href = Urls.Inscription;}}>Inscription</button> : <a/>}
                 {this.state.connectedUser !==undefined ? <button id="disconnect" className='menu_button' onClick={() => {this.setState({connectedUser : undefined})}}>Disconnect</button> : <a/>}
-                <a id="animal_all" className='menu_button' href='/animal/all'>Animal list&nbsp;|&nbsp;</a>
-                {this.state.connectedUser === undefined ? <a/> : <a id="favorites" className="menu_button" href={'/favorites'}>Favorites</a>}
+                <button id="animal_all" className='menu_button' onClick={() => {window.location.href = Urls.AnimalsList;}}>Animal list</button>
+                {this.state.connectedUser === undefined ? <a/> : <a id="favorites" className="menu_button" onClick={() => {window.location.href = Urls.Favorites;}}>Favorites</a>}
             </nav>
         )
     }
--- src/dist/roles.js
+++ src/dist/roles.js
@@ -1,5 +1,25 @@
-const Roles = {
+export const Roles = {
     Admin : "admin",
     User : "client",
+}
 
+export const User = () => {
+    return {
+        id : 0,
+        ndc : "Steduthu_admin",
+        mdp : "1532",
+        role : Roles.User,
+    }
+}
+
+export function getUser() {
+    let ans = undefined
+
+    fetch('/_api/connectedUser')
+    .then((res) => res.json())
+    .then((eventsReponse) => {
+        ans = eventsReponse
+    })
+
+    return ans
 }
