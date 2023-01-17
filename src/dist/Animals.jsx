c92165d4564936dc6c5c38ddcd00b8defc1922dc
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2023-01-17 08:18:39 +0100

    Maj

--- src/dist/Animals.jsx
+++ src/dist/Animals.jsx
@@ -1,3 +1,15 @@
 import ReactDOM from "react-dom";
 import Animal from "./animals";
 
+import {getAnimaux} from '../../database/db';
+import React from "react";
+
+function Animal(props) {
+    return <h1>{props.target}</h1>;
+}
+
+function AnimalsList() {
+    let animalsJSObj = getAnimaux();
+
+    return <li> { animalsJSObj.map( (Object) => <ul><Animal target={Object}/></ul> ) } </li>
+}
