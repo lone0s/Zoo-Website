cf10eb899c1050c6c5123620bb67a93ada3da938
Alessandro <alessandro@mail.fr>
2023-01-22 11:36:55 +0100

    suppression inutile

--- src/user.js
+++ src/user.js
@@ -1,6 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
 import {generateJWT} from './token.js';
-import {getUser, getUserByToken, deleteToken, addUserToken, findUser} from "../database/db.js";
 
 export const Roles = {
     Admin : "admin",
@@ -19,52 +17,5 @@ export function setUserCookie(idUtilisateur) {
     const joursExpiration = 1;
     d.setTime(d.getTime() + (joursExpiration*24*60*60*1000));
     let expires = "expires="+ d.toUTCString();
-    const token = generateJWT(idUtilisateur);
-    // Experimentation soso
-    // document.cookie = "test="+uuidv4()+";"+expires;
-    document.cookie = "test=" + token + ";" + expires;
-    addUserToken(idUtilisateur, token);
-    console.log(document.cookie);
-}
-
-export function getUserCookie() {
-    document.cookie.split(';').forEach((cookie) => {
-        const [key, value] = cookie.split('=');
-        if(key === "test") {
-            console.log(value);
-            return value;
-        }
-    });
-    return null;
-}
-
-export function isLoggedIn() {
-    let jwtSecret = process.env.JWT_SECRET_KEY;
-    let cookies = document.cookie.split(";");
-    let jwtCookie = cookies.find(c => c.trim().startsWith("test="));
-    if (jwtCookie) {
-        let jwt = jwtCookie.split("=")[1];
-        try {
-            let data = jwt.verify(jwt, jwtSecret);
-            console.log(data);
-            return getUserByToken(jwt);
-        } catch (error) {
-            console.log("Invalid JWT: ", error);
-            return false;
-        }
-    }
-    return false;
-}
-
-
-export function getConnectedUser() {
-    let ans = undefined
-
-    fetch('/_api/connectedUser')
-    .then((res) => res.json())
-    .then((eventsReponse) => {
-        ans = eventsReponse
-    })
-
-    return ans
+    document.cookie = "test="+generateJWT(idUtilisateur)+";"+expires;
 }
